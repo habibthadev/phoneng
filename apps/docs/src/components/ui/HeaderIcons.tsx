cf3299077007e082203079a300c779eb23aba0bc
habibthadev <adebayohabib7@gmail.com>
2026-04-14 11:17:55 +0100

    Add HeaderIcons UI component

---
+++ apps/docs/src/components/ui/HeaderIcons.tsx
@@ -0,0 +1,9 @@
+import { Phone, Github } from "lucide-react";
+
+export function PhoneIcon() {
+  return <Phone size={18} strokeWidth={2} fill="white" stroke="white" />;
+}
+
+export function GithubIcon() {
+  return <Github size={20} strokeWidth={2} />;
+}
